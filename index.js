620f3dce2980c6efc001f679d50c77b450943261
JGBrown <j.grant.brown@gmail.com>
2018-05-04 10:42:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = c3c3deb121cca574cfa2166fd7e9355100392d4c
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
